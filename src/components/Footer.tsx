1b71b0ee9114c5a43c106a68ba918c027fa44718
PickolZi <pickol876@gmail.com>
2025-02-08 18:02:33 -0800

    Added icons to footer links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,36 @@
 import Image from "next/image";
 import { ContentContainer } from "./ContentContainer";
 import Link from "next/link";
+import { FaDiscord, FaHome, FaInstagram, FaLinkedin } from "react-icons/fa";
+import { ReactElement } from "react";
+import { MdContactPhone, MdEventNote, MdTopic } from "react-icons/md";
+
+const HoverableLinkWithIcon = ({
+  text,
+  url,
+  icon,
+  newPage,
+}: {
+  text: string;
+  url: string;
+  icon: ReactElement;
+  newPage?: boolean;
+}) => {
+  return (
+    <Link href={url} target={newPage ? "_blank" : ""}>
+      <div className="flex flex-row float link items-center gap-[0.25rem]">
+        {icon}
+        <li>{text}</li>
+      </div>
+    </Link>
+  );
+};
 
 export const Footer = () => {
   return (
-    <div className="h-[10rem] md:h-[8rem] bg-background2 ">
+    <div className="bg-background2">
       <ContentContainer className="flex flex-row justify-between px-[1rem]">
-        <div className="flex flex-row items-center">
+        <div>
           <Link href="/">
             <Image
               src="/logos/TIPSE-logo-light-transparent.png"
@@ -19,37 +43,53 @@ export const Footer = () => {
           {/* <span>Technical Interview Preparation for Software Engineers</span> */}
         </div>
 
-        <div className="flex flex-row gap-[4rem]">
-          <ul className="flex flex-col justify-center">
-            <Link href="/">
-              <li className="float link">Home</li>
-            </Link>
-            <Link href="/topics">
-              <li className="float link">Topics</li>
-            </Link>
-            <Link href="/events">
-              <li className="float link">Events</li>
-            </Link>
-            <Link href="/contact-us">
-              <li className="float link">Contact Us</li>
-            </Link>
+        <div className="flex flex-row gap-[4rem] my-[1rem]">
+          {/* Pages Column */}
+          <ul className="flex flex-col">
+            <HoverableLinkWithIcon
+              text="Home"
+              url="/"
+              icon={<FaHome size={16} />}
+            />
+            <HoverableLinkWithIcon
+              text="Topics"
+              url="/topics"
+              icon={<MdTopic size={16} />}
+            />
+            <HoverableLinkWithIcon
+              text="Events"
+              url="/events"
+              icon={<MdEventNote size={16} />}
+            />
+            <HoverableLinkWithIcon
+              text="Contact Us"
+              url="/contact-us"
+              icon={<MdContactPhone size={16} />}
+            />
           </ul>
-          <ul className="flex flex-col justify-center">
-            <Link href="https://www.discord.gg" target="_blank">
-              <li className="float link">Discord</li>
-            </Link>
-            <Link
-              href="https://www.linkedin.com/in/tipse-calstatela/"
-              target="_blank"
-            >
-              <li className="float link">LinkedIn</li>
-            </Link>
-            <Link
-              href="https://www.instagram.com/tipse.calstatela"
-              target="_blank"
-            >
-              <li className="float link">Instagram</li>
-            </Link>
+
+          {/* Social Media Column */}
+          <ul className="flex flex-col">
+            <HoverableLinkWithIcon
+              text="Discord"
+              url="https://www.discord.gg"
+              icon={<FaDiscord size={16} />}
+              newPage={true}
+            />
+
+            <HoverableLinkWithIcon
+              text="LinkedIn"
+              url="https://www.linkedin.com/in/tipse-calstatela/"
+              icon={<FaLinkedin size={16} />}
+              newPage={true}
+            />
+
+            <HoverableLinkWithIcon
+              text="Instagram"
+              url="https://www.instagram.com/tipse.calstatela"
+              icon={<FaInstagram size={16} />}
+              newPage={true}
+            />
           </ul>
         </div>
       </ContentContainer>
